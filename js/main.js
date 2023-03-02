1ba2570a90e4cd3324e817262e4902504fa757c4
zahar500092 <100132137+zahar500092@users.noreply.github.com>
2023-03-02 21:24:28 +0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,21 +1,17 @@
 // Функция, возвращающая случайное целое число из переданного диапазона включительно
-let x=prompt("Введите первое число ");
-let y=prompt("Введите второе число ");
-let Random=function(x,y) {
-if (x<y){ return Math.floor((Math.random()*(y-x+1))+x)}
-if (x>y){ return Math.floor((Math.random()*(x-y+1))+y)}
+function random(x, y) {
+  const z = x - 0.5 + Math.random() * (y - x + 1);
+  return Math.round(z);
 }
-{ console.log('Рандомное число '+ Random(x,y)) }
+
+console.log ( 'Рандомное число',random(10, 110) );
 //
 
-
 //Функция для проверки максимальной длины строки
-let str = prompt("Введите строку ");
-let maxlong = 140;
 function MaxLine(str,maxlong) {
-  str=str.length
- if (str<=maxlong) {console.log(true)} 
- else {console.log(false)}
-  }
-MaxLine(str,maxlong);
+  if (str.length<=maxlong) {console.log('true');}
+  else {console.log('false');}
+}
+MaxLine('Пример коментария ',140);
+
 //
