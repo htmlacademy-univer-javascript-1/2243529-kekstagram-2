a219cfff42fbdf248c7ef465d66c30c3cf945b30
Keks <keks@htmlacademy.ru>
2023-03-02 19:26:51 +0300

    :heavy_check_mark: Сборка #4

---
+++ 4/js/main.js
@@ -0,0 +1,17 @@
+// Функция, возвращающая случайное целое число из переданного диапазона включительно
+function random(x, y) {
+  const z = x - 0.5 + Math.random() * (y - x + 1);
+  return Math.round(z);
+}
+
+console.log ( 'Рандомное число',random(10, 110) );
+//
+
+//Функция для проверки максимальной длины строки
+function MaxLine(str,maxlong) {
+  if (str.length<=maxlong) {console.log('true');}
+  else {console.log('false');}
+}
+MaxLine('Пример коментария ',140);
+
+//
