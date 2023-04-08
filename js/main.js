07f1e3a0e4313ff36a00c5bb2fa3c5dd0171a1b5
zahar500092 <100132137+zahar500092@users.noreply.github.com>
2023-04-08 12:53:41 +0500

    7.15. Отрисуй меня полностью (часть 1)

---
+++ js/main.js
@@ -0,0 +1,21 @@
+import {INdescription} from '/data.js';
+import {INname} from '/data.js';
+import {INmessage} from '/data.js';
+import {random} from '/utils.js';
+import {MadeCommentId} from '/utils.js';
+
+const MadeComment=()=> ({
+  id:MadeCommentId(),
+  avatar: `img/avatar-${random(1,6)}.svg`,
+  name: INname[random(0, INname.length-1)],
+  message:INmessage[random(0, INmessage.length-1)],
+});
+
+const MadeMainArray=(id)=> ({
+  id:id,
+  url: `photos/${id}.jpg`,
+  descrition: INdescription[random(0,INdescription.length-1)],
+  likes:random(15,200),
+  comments:Array.from({length:random(1,5)}, MadeComment)
+});
+export{MadeMainArray};
